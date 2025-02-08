71fea75ee8ab416ce38052dd7e07080faa7a9399
Lucas <lucasvida2005@gmail.com>
2025-02-08 00:51:10 -0300

    Addding Progress Logic

--- app/src/main/java/com/example/financetracker/features/composables/InvestmentCard.kt
+++ app/src/main/java/com/example/financetracker/features/composables/InvestmentCard.kt
@@ -4,10 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
@@ -53,17 +55,6 @@ fun InvestmentCard() {
                 text = "$14,850.00",
                 style = MaterialTheme.typography.headlineMedium
             )
-
-            Column {
-                Text(
-                    text = "Goal $20,000.00",
-                    style = MaterialTheme.typography.titleSmall,
-                    color = Color.Gray
-                )
-                Button(
-                    onClick = {},
-                ) { }
-            }
         }
     }
 }
